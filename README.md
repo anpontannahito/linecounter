1d4fc56c5e35345a384521aab4856c1f023c6787
anpontannahito <tabetataytomato@gmail.com>
2026-02-06 19:59:23 +0900

    追加
    ファイルの追加

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# linecounter
-C language program to count the number of lines in a file
+# name
+LineCounter
+
+## Overview
+C言語で作られた行数カウンターです。
+
+## How to start
+ - exeファイルから起動する方法
+ 1. GithubのReleasesからファイルをダウンロードする
+ 2. ターミナルから実行
+ - ソースコードから起動する方法
+ 1. GCCでコンパイルして生成されたexeファイルをターミナルで実行
+
+## Usage
+1. ターミナルに```linecounter.exe <ファイルパス>```を指定して実行
+
+## Features
+- ファイルサイズの表示
+- 行数の表示
+
+## Author
+- [Github](https://github.com/anpontannahito)
+- [ホームページ](https://anpontannahito.github.io/)
+
+## Licence
+[MIT License](https://opensource.org/license/mit)<br>
+このソフトウェアはMIT Licenseの条件の下で配布されます。
--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+
+fpos_t Getfilesize(const char* Filepath);
 
 int main(int argc, char *argv[]) {
+    int linecount = 1;
+    int c;
     if (argc < 2){
         printf("Usage: %s <filepath>\n", argv[0]);
         return 1;
@@ -11,17 +14,27 @@ int main(int argc, char *argv[]) {
     char *filepath = argv[1];
     file = fopen(filepath, "r");
     if (file == NULL){
-        printf("file open error : %s\n",filepath);
+        printf("File open error : %s\n",filepath);
         return 1;
     }
     else {
-        printf("file opened successfully: %s\n", filepath);
-        fclose(file);
+        printf("File opened successfully: %s\n", filepath);
     }
+    fpos_t filesize = Getfilesize(filepath);
+    printf("File size: %ld bytes\n", filesize);
+
+    while ((c = fgetc(file)) != EOF){
+        if (c == '\n'){
+            linecount++;
+        }
+    }
+
+    fclose(file);
+    printf("Total lines: %d\n", linecount);
     return 0;
 }
 
-fpos_t Getfilesize(const char* Filepath[]){
+fpos_t Getfilesize(const char* Filepath){
     fpos_t fsize = 0;
     FILE *file = fopen(Filepath, "rb");
 
