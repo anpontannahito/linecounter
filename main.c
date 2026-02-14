740bfce4bd9abf40ce299cbf636c52e0a7371020
anpontannahito <tabetataytomato@gmail.com>
2026-02-15 00:13:44 +0900

    追加
    複数ファイルを読み込みを追加

--- main.c
+++ main.c
@@ -1,47 +1,57 @@
 #include <stdio.h>
 #include <string.h>
 
-fpos_t Getfilesize(const char* Filepath);
+long Getfilesize(FILE *file,char *filepath);
+int linecounter(FILE *file,char *filepath);
 
 int main(int argc, char *argv[]) {
-    int linecount = 1;
-    int c;
+    int total_linecount = 0;
+    long total_size = 0;
     if (argc < 2){
         printf("Usage: %s <filepath>\n", argv[0]);
         return 1;
     }
-    FILE *file;
-    char *filepath = argv[1];
-    file = fopen(filepath, "r");
-    if (file == NULL){
-        printf("File open error : %s\n",filepath);
-        return 1;
+    for (int i = 1;i < argc; i++){
+        FILE *file;
+        file = fopen(argv[i], "r");
+        if (file == NULL){
+            printf("File open error: %s\n", argv[i]);
+            continue;
+        }
+        long size = Getfilesize(file, argv[i]);
+        int linecount = linecounter(file, argv[i]);
+        fclose(file);
+        total_size = total_size + size;
+        total_linecount = total_linecount + linecount;
     }
-    else {
-        printf("File opened successfully: %s\n", filepath);
-    }
-    fpos_t filesize = Getfilesize(filepath);
-    printf("File size: %ld bytes\n", filesize);
+    printf("\n=== TOTAL ===\n");
+    printf("Total file size: %ld bytes\n",total_size);
+    printf("Total lines: %d line\n", total_linecount);
+    return 0;
+}
 
+int linecounter(FILE *file,char *filepath){
+    int linecount = 0;
+    int c;
+    int last_char = '\0';
+    rewind(file);
     while ((c = fgetc(file)) != EOF){
         if (c == '\n'){
             linecount++;
         }
+        last_char = c;
+    }
+    if (last_char != '\n' && last_char != '\0'){
+        linecount++;
     }
 
-    fclose(file);
-    printf("Total lines: %d\n", linecount);
-    return 0;
+    printf("%s lines: %d line\n", filepath, linecount);
+    return linecount;
 }
 
-fpos_t Getfilesize(const char* Filepath){
-    fpos_t fsize = 0;
-    FILE *file = fopen(Filepath, "rb");
-
+long Getfilesize(FILE *file,char *filepath){
     fseek(file, 0, SEEK_END);
-    fgetpos(file, &fsize);
-
-    fclose(file);
-
-    return fsize;
+    long size = ftell(file);
+    printf("%s size: %ld bytes\n", filepath, size);
+    return size;
 }
